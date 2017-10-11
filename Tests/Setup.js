c11de50f8aa27493f8c3f08275bf6dc15c92ac53
liam humphreys <liam.humphreys1989@gmail.com>
2017-10-11 15:26:50 +0100

    mock

--- Tests/Setup.js
+++ Tests/Setup.js
@@ -21,3 +21,15 @@ jest
     }
   }
 })
+.mock('TextInput', () => {
+  const RealComponent = require.requireActual('TextInput');
+  const React = require('React');
+  class TextInput extends React.Component {
+    render() {
+      delete this.props.autoFocus;
+      return React.createElement('TextInput', this.props, this.props.children);
+    }
+  }
+  TextInput.propTypes = RealComponent.propTypes;
+  return TextInput;
+});
